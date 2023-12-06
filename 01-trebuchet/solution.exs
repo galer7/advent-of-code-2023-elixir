574fa61f4533cb55ea01ed315bd9929245cc2434
galer7 <gabigaler50@gmail.com>
2023-12-06 09:07:42 +0200

    Rename variables for day 1 solution

--- 01-trebuchet/solution.exs
+++ 01-trebuchet/solution.exs
@@ -1,40 +1,40 @@
+# Problem text: https://adventofcode.com/2023/day/1
+
 defmodule Trebuchet do
   def solve do
-    input_lines = read_file(relative_from_here("input.txt"))
+    calibration_doc_lines = read_file(relative_from_here("input.txt"))
 
-    input_lines
-    |> Enum.map(&get_first_and_last_digit_from_string/1)
+    calibration_doc_lines
+    |> Enum.map(&get_calibration_value/1)
     |> Enum.sum()
     # prints result to stdout
     |> IO.puts()
   end
 
-  defp get_first_and_last_digit_from_string(string) do
-    IO.puts("[get_first_and_last_digit_from_string] string: #{string}")
+  defp get_calibration_value(line) do
+    IO.puts("[get_calibration_value] line: #{line}")
 
     first_search_result =
       Regex.named_captures(
         ~r/^[^\d]*(?<first_digit>[\d]{1}).*(?<last_digit>[\d]{1})[^\d]*$/,
-        string
+        line
       )
 
-    IO.puts(
-      "[get_first_and_last_digit_from_string] first_search_result: #{inspect(first_search_result)}"
-    )
+    IO.puts("[get_calibration_value] first_search_result: #{inspect(first_search_result)}")
 
     case first_search_result do
       nil ->
-        %{"digit" => digit} = get_only_digit_from_string(string)
+        %{"digit" => digit} = get_only_digit_from_string(line)
         {result, _} = Integer.parse("#{digit}#{digit}")
 
-        IO.puts("[get_first_and_last_digit_from_string] result: #{inspect(result)}")
+        IO.puts("[get_calibration_value] result: #{inspect(result)}")
         result
 
       _ ->
         %{"first_digit" => first_digit, "last_digit" => last_digit} = first_search_result
         {result, _} = Integer.parse("#{first_digit}#{last_digit}")
 
-        IO.puts("[get_first_and_last_digit_from_string] result: #{inspect(result)}")
+        IO.puts("[get_calibration_value] result: #{inspect(result)}")
         result
     end
   end
